8dd2065e30853c4149566530a27713aaa9b8dd3a
Daniel Melo <o.danielworkmelo@gmail.com>
2023-02-25 22:03:24 +0000

    Proj Completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,4 @@
-document.body.addEventListener('keyup', (e) => {
-    playSound(e.code.toLowerCase());
-})
-
+//Funções
 function playSound(sound){
     let audioEl = document.querySelector(`#s_${sound}`);
     let keyEl = document.querySelector(`div[data-key="${sound}"]`);
@@ -9,13 +6,39 @@ function playSound(sound){
     if(audioEl){
         audioEl.currentTime = 0;//Zerando o audio
         audioEl.play();
-    }
+    };
 
     if(keyEl){
         keyEl.classList.add('active');
 
         setTimeout(() => {
             keyEl.classList.remove('active')
-        }, 400)
+        }, 400);
+    };
+}
+
+function playComposition(songArr){
+    let wait = 0;
+
+    for(let songItem of songArr) {
+        setTimeout(() => {
+            playSound(`key${songItem}`);
+        }, wait);
+        
+        wait += 250;
     }
 }
+
+//Eventos
+document.body.addEventListener('keyup', (e) => {
+    playSound(e.code.toLowerCase());
+});
+
+document.querySelector('.composer button').addEventListener('click', () => {
+    let song = document.querySelector('#input').value;
+
+    if(song !== ''){
+        let songArr = song.split('');
+        playComposition(songArr);
+    }
+});
